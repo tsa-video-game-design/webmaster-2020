5492fbf2fab5531dc8efbec5616fa490550a25cb
AwesomeIndustry <22535264+AwesomeIndustry@users.noreply.github.com>
2020-02-10 20:46:33 -0800

    Testing CTE accelerometer parallax

--- cte.js
+++ cte.js
@@ -9,9 +9,26 @@ window.onload = function() {
     let sensor = new Accelerometer();
     sensor.start();
     sensor.onreading = () => {
+        /* X RANGE: -10 <= x <= 10 */
         console.log("Acceleration along X-axis: " + sensor.x);
+        /* Y RANGE: -10 <= y <= 10 */
         console.log("Acceleration along Y-axis: " + sensor.y);
         console.log("Acceleration along Z-axis: " + sensor.z);
+
+
+        var setOffset = function(selector,left,top) {
+            $(selector).css({top: top, left: left});
+        }
+
+        var Xdistance = ((sensor.x + 10) / 20);
+        var Ydistance = ((sensor.x + 10) / 20);
+        setOffset(".bg1",Xdistance * 20,Ydistance * 20);
+        setOffset(".bg2",Xdistance * 15,Ydistance * 15);
+        setOffset(".bg3",Xdistance * 10,Ydistance * 10);
+        setOffset(".bg4",Xdistance * 5,Ydistance * 5);
+        setOffset(".bg5",Xdistance * -5,Ydistance * -5);
+        setOffset(".bg6",Xdistance * -10,Ydistance * -10);
+        setOffset(".bg7",Xdistance * -15,Ydistance * -15);
     }
 };
 window.onmousemove = function(e) {
