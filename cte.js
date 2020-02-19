d3c3c269419ee2835e94ca3dfa95dc8b3c354f35
AwesomeIndustry <22535264+AwesomeIndustry@users.noreply.github.com>
2020-02-19 16:58:59 -0700

    Added about page, icons on CTE page, misc stuff

--- cte.js
+++ cte.js
@@ -1,43 +1,40 @@
-window.onload = function() {
+window.addEventListener("load", function() {
     setTimeout(function() {
         /* Maybe fade each layer in individually */
         $(".intro-hero-bg").animate({
             opacity: 0.7
         },1000);
+
+
+
     },1700);
-
-    let sensor = new Accelerometer();
-    sensor.start();
-    sensor.onreading = () => {
-        /* X RANGE: -10 <= x <= 10 */
-        console.log("Acceleration along X-axis: " + sensor.x);
-        /* Y RANGE: -10 <= y <= 10 */
-        console.log("Acceleration along Y-axis: " + sensor.y);
-        console.log("Acceleration along Z-axis: " + sensor.z);
+    if (navigator.userAgent.indexOf("Android") > -1 || navigator.userAgent.indexOf("iPhone") > -1 || navigator.userAgent.indexOf("Mobile") > -1) {
+        console.log("Starting sensor...");
+        let sensor = new Accelerometer();
+        sensor.start();
+        /*
+        sensor.onreading = () => {
 
 
-        var setOffset = function(selector,left,top) {
-            $(selector).css({top: top, left: left});
-        }
+            var setOffset = function(selector,left,top) {
+                $(selector).css({top: top, left: left});
+            }
 
-        var Xdistance = ((sensor.x + 10) / 20);
-        var Ydistance = ((sensor.x + 10) / 20);
-        setOffset(".bg1",Xdistance * 20,Ydistance * 20);
-        setOffset(".bg2",Xdistance * 15,Ydistance * 15);
-        setOffset(".bg3",Xdistance * 10,Ydistance * 10);
-        setOffset(".bg4",Xdistance * 5,Ydistance * 5);
-        setOffset(".bg5",Xdistance * -5,Ydistance * -5);
-        setOffset(".bg6",Xdistance * -10,Ydistance * -10);
-        setOffset(".bg7",Xdistance * -15,Ydistance * -15);
+            var Xdistance = ((sensor.x + 10) / 20);
+            var Ydistance = ((sensor.x + 10) / 20);
+            updateParallax(Xdistance,Ydistance);
+        }*/
+        window.ondevicemotion = function() {
+            var Xdistance = ((sensor.x + 10) / 20);
+            var Ydistance = ((sensor.x + 10) / 20);
+            updateParallax(Xdistance,Ydistance);
+        };
     }
-};
-window.onmousemove = function(e) {
+});
+function updateParallax(Xdistance, Ydistance) {
     var setOffset = function(selector,left,top) {
         $(selector).css({top: top, left: left});
     }
-
-    var Xdistance = -(1 - e.clientX / window.innerWidth);
-    var Ydistance = (e.clientY / window.innerHeight);
     setOffset(".bg1",Xdistance * 20,Ydistance * 20);
     setOffset(".bg2",Xdistance * 15,Ydistance * 15);
     setOffset(".bg3",Xdistance * 10,Ydistance * 10);
@@ -46,3 +43,8 @@ window.onmousemove = function(e) {
     setOffset(".bg6",Xdistance * -10,Ydistance * -10);
     setOffset(".bg7",Xdistance * -15,Ydistance * -15);
 }
+window.addEventListener("mousemove",function(e) {
+    var Xdistance = -(1 - e.clientX / window.innerWidth);
+    var Ydistance = (e.clientY / window.innerHeight);
+    updateParallax(Xdistance,Ydistance);
+});
--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
-window.onload = function() {
+window.addEventListener("load",function() {
     setupNavigationControls();
-    setupHoverTooltip();
-};
+    //setupHoverTooltip();
+});
 function setupNavigationControls() {
-
     var collapseMenu = function(animation_speed) {
         $("#nav-expanded").animate({
             left: "-3em",
@@ -44,101 +43,5 @@ function setupNavigationControls() {
 
     $("#nav-shade").click(function(e) {
         collapseMenu(170);
-    })
-}
-function setupHoverTooltip() {
-    var globalMouseX = 0;
-    var globalMouseY = 0;
-    function refreshTooltipPosition() {
-        var target = document.getElementsByClassName("hover-tooltip")[0];
-        if (globalMouseX > (document.body.clientWidth - target.clientWidth)) {
-            target.style.left = (document.body.clientWidth - target.clientWidth + 1) + "px";
-        } else {
-            target.style.left = globalMouseX+"px";
-        }
-        target.style.top = (globalMouseY - target.clientHeight - 5)+"px";
-    }
-    window.onmousemove = function(e) {
-        globalMouseX = e.clientX;
-        globalMouseY = e.clientY;
-        refreshTooltipPosition();
-    };
-    $(".hotspot").on("mouseenter", function(e) {
-        var className = e.target.className.match(/^[^\s]+ ([^\s]+)/)[1];
-        var title;
-        var description;
-        console.log(className);
-        if (className == "hotspot-1") {
-            title = "Advanced Mirror Cameras";
-            description = `Cameras integrated into the mirrors allow the car to gain a full picture of the surroundings, including blind spots.`;
-        } else if (className == "hotspot-2") {
-            title = "Headlight Beam Forming";
-            description = `This advanced technology integrates infared LEDs in the headlights along with the infared camera array to sense the road conditions around the vehicle.`;
-        } else if (className == "hotspot-3") {
-            title = "Roof-mounted radar";
-            description = `Integrated lidar sensors in the spoiler of the car allow our advanced A.I. driving algorithms to identify road signs and objects far away.`;
-        } else if (className == "hotspot-4") {
-            title = "Infared Camera Array";
-            description = `These cameras, integrated inside the grille, capture infared light beamed from the headlights. This allows the car to sense road conditions.`;
-        } else {
-            title = "Depth-sensing Tire Tread";
-            description = `These depth-sensing tire treads automatically keep track of road terrain and train our A.I. model to better recognize and respond to road conditions as you drive.`;
-        }
-
-        $(".hover-tooltip h1").text(title);
-        $(".hover-tooltip .tooltip-info").text(description);
-
-        $(".hover-tooltip").show();
-        $(".hover-tooltip").animate({
-            opacity: 1
-        }, {
-            duration: 100
-        });
-    });
-    $(".hotspot").on("mouseout", function(e) {
-        $(".hover-tooltip").animate({
-            opacity: 0
-        }, {
-            duration: 50
-        });
-    });
-    $(".hotspot").on("click", function(e) {
-        var className = e.target.className.match(/^[^\s]+ ([^\s]+)/)[1];
-
-        var updateTexts = function(className) {
-            var title;
-            var description;
-            if (className == "hotspot-1") {
-                title = "Advanced Mirror Cameras";
-                description = `Cameras integrated into the mirrors allow the car to gain a full picture of the surroundings, including blind spots.`;
-            } else if (className == "hotspot-2") {
-                title = "Headlight Beam Forming";
-                description = `This advanced technology integrates infared LEDs in the headlights along with the infared camera array to sense the road conditions around the vehicle.`;
-            } else if (className == "hotspot-3") {
-                title = "Roof-mounted radar";
-                description = `Integrated lidar sensors in the spoiler of the car allow our advanced A.I. driving algorithms to identify road signs and objects far away.`;
-            } else if (className == "hotspot-4") {
-                title = "Infared Camera Array";
-                description = `These cameras, integrated inside the grille, capture infared light beamed from the headlights. This allows the car to sense road conditions.`;
-            } else {
-                title = "Depth-sensing Tire Tread";
-                description = `These depth-sensing tire treads automatically keep track of road terrain and train our A.I. model to better recognize and respond to road conditions as you drive.`;
-            }
-            $(".mobile-tooltip h1").text(title);
-            $(".mobile-tooltip .mobile-tooltip-info").text(description);
-        };
-
-        if ($(".mobile-tooltip .intro").is(":visible")) {
-            $(".mobile-tooltip .intro").fadeOut(200, function() {
-                updateTexts(className);
-                $(".mobile-tooltip h1").fadeIn(200);
-                $(".mobile-tooltip .mobile-tooltip-info").fadeIn(200);
-            });
-        } else {
-            $(".mobile-tooltip h1, .mobile-tooltip .mobile-tooltip-info").fadeOut(200, function() {
-                updateTexts(className);
-                $(".mobile-tooltip h1, .mobile-tooltip .mobile-tooltip-info").fadeIn(200);
-            })
-        }
     });
 }
